f288ad8b94228f551180272801c598408ac34020
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 20:56:57 +0800

    description

---
+++ exercise_8.py
@@ -0,0 +1,12 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 8:
+# Print the following pattern
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5
+
